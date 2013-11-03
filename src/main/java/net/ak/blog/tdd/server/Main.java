90586d0a1294e827b5af773b92d1ed118ef0e2f9
Akira Koyasu <mail@akirakoyasu.net>
2013-11-04 00:32:23 +0900

    init project

---
+++ src/main/java/net/ak/blog/tdd/server/Main.java
@@ -0,0 +1,10 @@
+package net.ak.blog.tdd.server;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
